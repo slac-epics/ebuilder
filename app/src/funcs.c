a3ef863ebac98dc200255b7b05f713d38273b2bc
Michael Browne <mcbrowne@slac.stanford.edu>
2023-02-13 09:47:49 -0800

    LCLS2-ize.  We look at timestamps only now, and don't assume they contain a fiducial.

--- app/src/funcs.c
+++ app/src/funcs.c
@@ -13,20 +13,18 @@
 #include<string.h>
 #include<strings.h>
 
-#define LCLS1_FID_MAX        0x1ffe0
-#define LCLS1_FID_ROLL_LO    0x00200
-#define LCLS1_FID_ROLL_HI    (LCLS1_FID_MAX-LCLS1_FID_ROLL_LO)
-#define LCLS1_FID_ROLL(a,b)  ((b) < LCLS1_FID_ROLL_LO && (a) > LCLS1_FID_ROLL_HI)
-#define LCLS1_FID_GT(a,b)    (LCLS1_FID_ROLL(b, a) || ((a) > (b) && !LCLS1_FID_ROLL(a, b)))
+/* We assume this is a power of two. */
+#define EBSIZE 1024
 
 struct eb_pvt {
-    double *data[LCLS1_FID_MAX];        /* Built arrays of data */
-    epicsTimeStamp ts[LCLS1_FID_MAX];   /* The corresponding timestamps */
-    int mask[LCLS1_FID_MAX];            /* Which pieces of data do we have? */
-    int off[21];                  /* Offsets into data for each input. */
-    int all;                      /* Mask for all of the data */
-    int cnt;                      /* How many inputs do we have? */
-    int size;                     /* The total size of the data we are sending */
+    int cur;                            /* The most recent timestamp we have received. */
+    double *data[EBSIZE];               /* Built arrays of data */
+    epicsTimeStamp ts[EBSIZE];          /* The corresponding timestamps */
+    int mask[EBSIZE];                   /* Which pieces of data do we have? */
+    int off[21];                        /* Offsets into data for each input. */
+    int all;                            /* Mask for all of the data */
+    int cnt;                            /* How many inputs do we have? */
+    int size;                           /* The total size of the data we are sending */
 };
 
 long eventBuildInit(struct aSubRecord *psub)
@@ -59,15 +57,16 @@ long eventBuildInit(struct aSubRecord *psub)
     }
     /* OK, looks good.  Let's initialize our private structure! */
     pvt = (struct eb_pvt *)malloc(sizeof(*pvt));
-    pvt->data[0] = (double *)malloc(cnt * LCLS1_FID_MAX * sizeof(double));
-    bzero(pvt->data[0], cnt * LCLS1_FID_MAX * sizeof(double));
+    pvt->cur = 0;
+    pvt->data[0] = (double *)malloc(cnt * EBSIZE * sizeof(double));
+    bzero(pvt->data[0], cnt * EBSIZE * sizeof(double));
     bzero(pvt->ts, sizeof(pvt->ts));
     bzero(pvt->mask, sizeof(pvt->mask));
     bcopy(off, pvt->off, sizeof(off));
     pvt->all = (1 << i) - 1;
     pvt->cnt = i;
     pvt->size = cnt;
-    for (i = 1; i < LCLS1_FID_MAX; i++)
+    for (i = 1; i < EBSIZE; i++)
 	pvt->data[i] = pvt->data[i-1] + cnt;
     psub->dpvt = pvt;
     return 0;
@@ -80,34 +79,58 @@ long eventBuild(struct aSubRecord *psub)
     epicsFloat64 **d = (epicsFloat64 **)&psub->a;
     epicsUInt32 *no = &psub->noa;
     int i;
-    int lastfid = -1;
+    int lastidx = -1;
     if (!pvt)
 	return 0; /* Some problem, just skip! */
     for (i = 0; i < pvt->cnt; i++, inp++, no++, d++) {
 	epicsTimeStamp ts;
-	int fid;
+	int idx = pvt->cur;
 	dbGetTimeStamp(inp, &ts);
-	fid = ts.nsec & 0x1ffff;
-	if (pvt->ts[fid].nsec != ts.nsec || 
-	    pvt->ts[fid].secPastEpoch != ts.secPastEpoch) {
-	    /* A brand new timestamp! */
-	    pvt->ts[fid] = ts;
-	    pvt->mask[fid] = 0;
+	/*
+	 * Search for this timestamp.  We're hoping it's either
+	 * new or the most recent.
+	 */
+	if (pvt->ts[idx].secPastEpoch < ts.secPastEpoch ||
+	    (pvt->ts[idx].secPastEpoch == ts.secPastEpoch &&
+	     pvt->ts[idx].nsec < ts.nsec)) {
+	    /* This data is more recent, make a new entry!! */
+	    idx = (idx+1) & (EBSIZE-1);
+	    pvt->ts[idx] = ts;
+	    pvt->mask[idx] = 0;
+	    pvt->cur = idx;
+	} else if (pvt->ts[idx].secPastEpoch > ts.secPastEpoch ||
+		   (pvt->ts[idx].secPastEpoch == ts.secPastEpoch &&
+		    pvt->ts[idx].nsec > ts.nsec)) {
+	    /* This data is older than the current. */
+	    do {
+		idx = idx ? (idx-1) : (EBSIZE-1);
+	    } while ((pvt->ts[idx].secPastEpoch > ts.secPastEpoch ||
+		      (pvt->ts[idx].secPastEpoch == ts.secPastEpoch &&
+		       pvt->ts[idx].nsec > ts.nsec)) &&
+		     idx != pvt->cur);
+	    if (pvt->ts[idx].secPastEpoch != ts.secPastEpoch ||
+		pvt->ts[idx].nsec != ts.nsec)
+		continue; /* If we didn't find a match, give up! */
 	}
-	if (!(pvt->mask[fid] & (1<<i))) {
+	/* The third case is cur *is* our timestamp! */
+	if (!(pvt->mask[idx] & (1<<i))) {
 	    /* We don't have this data yet. */
-	    bcopy(*d, pvt->data[fid] + pvt->off[i], (*no) * sizeof(double));
-	    pvt->mask[fid] |= 1<<i;
-	    if (pvt->mask[fid] == pvt->all) {
-		if (lastfid == -1 || LCLS1_FID_GT(fid, lastfid)) {
-		    lastfid = fid;
+	    bcopy(*d, pvt->data[idx] + pvt->off[i], (*no) * sizeof(double));
+	    pvt->mask[idx] |= 1<<i;
+	    if (pvt->mask[idx] == pvt->all) { /* We have everything! */
+		if (lastidx == -1 || 
+		    (pvt->ts[lastidx].secPastEpoch < ts.secPastEpoch ||
+		     (pvt->ts[lastidx].secPastEpoch == ts.secPastEpoch &&
+		      pvt->ts[lastidx].nsec < ts.nsec))) {
+		    /* If this is our first completion, or it's more recent, send it! */
+		    lastidx = idx; 
 		}
 	    }
 	}
     }
-    if (lastfid >= 0) {
-	bcopy(pvt->data[lastfid], psub->vala, pvt->size * sizeof(double));
-	bcopy(&pvt->ts[lastfid], &psub->time, sizeof(epicsTimeStamp));
+    if (lastidx >= 0) {
+	bcopy(pvt->data[lastidx], psub->vala, pvt->size * sizeof(double));
+	bcopy(&pvt->ts[lastidx], &psub->time, sizeof(epicsTimeStamp));
 	psub->neva = pvt->size;
     }
     return 0;
